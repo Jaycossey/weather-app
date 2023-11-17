ac4cb470b23461293a1ba2b69220ca54cda1effb
jaycossey <jaycossey@live.com>
2023-11-17 17:45:20 +0000

    arranged and styled data on page, need fix for remove current widget in search. need style for searched weather container

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,5 +1,8 @@
 // GLOBALS --------------------------------------------------------------------------------
 
+// Searchbar
+const searchBar = document.getElementById('searchLoc');
+
 // Containers
 const localWeatherContainer = document.getElementById('localWeather');
 const searchedWeatherContainer = document.getElementById('searchedWeather');
@@ -129,6 +132,10 @@ async function fetchLatLon(city) {
 
 // MARS WEATHER API CALL --------------------------------------------------------------------
 
+// fetch mars weather
+function fetchMarsWeather() {
+    console.log("Mars Weather! Assign this function!");
+}
 
 // DATE TIME FUNCTIONS ----------------------------------------------------------------------
 
@@ -156,6 +163,10 @@ function displayDateTime(element, format) {
 
 // FUNCTIONS TO DISPLAY DATA ----------------------------------------------------------------
 
+
+// Position of weather widget on screen
+let widgetPosition = "Local";
+
 // Widget Array (lower api call quantity)
 let widgetArray = [];
 
@@ -164,19 +175,97 @@ class Widget {
     constructor(location, weather, temp, humidity, wind) {
         this.location = location;
         this.weather = weather;
-        this.temp = temp;
-        this.humidity = humidity;
-        this.wind = wind;
+        this.temp = "Temperature (celcius): " + temp;
+        this.humidity = "Humidity: " + humidity;
+        this.wind = "Wind Speed: " + wind;
     }
+}
+
+function clearCurrentWidget() {
+    // // if the element doesnt have child nodes, return
+    // if (searchedWeatherContainer.hasChildNodes()) return;
+    // console.log("true");
+
+    
+    // return;
+
+    // Does element have children
+    if (!searchedWeatherContainer.hasChildNodes()) {
+        console.log("true");
+        let currentWidget = searchedWeatherContainer.querySelector('div');
+        console.log(currentWidget);
+        searchedWeatherContainer.removeChild(currentWidget);
+        return;
+    } else {
+        console.log("False");
+        return;
+    }
+    
+
 }
 
 // weather widget function
 function createWidget() {
-    widgetArray.push(new Widget(locationName, weatherType, temperatureNum, humidityPer, windSpeed));
+    
+    // create and store objects in array
+    widgetArray.unshift(new Widget(locationName, weatherType, temperatureNum, humidityPer, windSpeed));
     console.log(widgetArray);
+    
+    // create parent div
+    let widgetEl = document.createElement('div');
+    widgetEl.className = "widget";
+    widgetEl.style.width = "100%";
+    widgetEl.style.height = "50%";
+
+    // Create elements to display data
+    let locationHeader = document.createElement('h2');
+    let weatherSubHeader = document.createElement('h3');
+    let tempEl = document.createElement('p');
+    let humidityEl = document.createElement('p');
+    let windEl = document.createElement('p');
+
+    // assign content to elements
+    locationHeader.innerText = widgetArray[0].location;
+    weatherSubHeader.innerText = widgetArray[0].weather;
+    tempEl.innerHTML = widgetArray[0].temp;
+    humidityEl.innerText = widgetArray[0].humidity;
+    windEl.innerText = widgetArray[0].wind;
+
+    // append weather widgets
+    widgetEl.append(locationHeader, weatherSubHeader, tempEl, humidityEl, windEl);
+
+    // Where to display weather
+    if (widgetPosition === "Local") {
+        localWeatherContainer.append(widgetEl);
+    } else {
+        searchedWeatherContainer.append(widgetEl);
+    }
+
 }
+
+
+// EVENT LISTENERS --------------------------------------------------------------------------
+
+// Search bar listener and handler
+searchBar.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        if (searchBar.value == 'mars') {
+            fetchMarsWeather();
+            return;
+        }
+        // remove current widget from the screen (if present)
+        clearCurrentWidget();
+        // set position to align widget in correct section
+        widgetPosition = "Searched";
+        // search for closest match to search value
+        fetchLatLon(searchBar.value);
+    }
+    
+})
+
 
 displayDateTime(navDate, "date");
 displayDateTime(navTime, "time");
 
-document.onload = fetchLatLon("Northampton");
+document.onload = fetchLatLon("London");
+
